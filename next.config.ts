9581c8c10e8ee5727ae1843a3b0f09ea04b692e3
Duncan Maina <duncandevs@gmail.com>
2025-01-13 16:04:24 -0600

    ignore lint errors

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
