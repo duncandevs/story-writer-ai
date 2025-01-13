13ce85a89d0c4b6a87081591a9917fd237a932a5
Duncan Maina <duncandevs@gmail.com>
2025-01-13 13:07:34 -0600

    fixing chapter title width

--- src/components/editor/StoryDrawerList.tsx
+++ src/components/editor/StoryDrawerList.tsx
@@ -23,13 +23,13 @@ export const StoryDrawerList: React.FC<StoryDrawerListProps> = ({ story, classNa
     };
 
     return (
-        <ul className={cn('ChapterList flex flex-col gap-4 w-inherit', className)}>
+        <ul className={cn('ChapterList flex flex-col gap-4', className)}>
             {story?.chapters?.map((chapter, idx)=>(<li key={`chapter-${idx}`}>
                 <Collapsible>
-                    <div className="flex hover:bg-amber-200 rounded-md group">
+                    <div className="flex hover:bg-amber-200 rounded-md group w-[272px]">
                         <CollapsibleTrigger className="w-full">
                             <div className="flex w-full p-2 pr-4 group">
-                                <p className="text-xl">{chapter.title}</p>
+                                <p className="text-lg">{chapter.title}</p>
                             </div>
                         </CollapsibleTrigger>
                         <button className="p-2 pl-4 opacity-0 group-hover:opacity-100" onClick={()=>handleCreateNewPage(chapter.id)}>
